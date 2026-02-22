243bc7d7954dfddfcbfb1f9ed4e686ee757aa1b7
NekhLanguage <nekhbrazil@gmail.com>
2026-02-22 10:07:24 +0100

    Update languages.js

--- languages.js
+++ languages.js
@@ -3,6 +3,6 @@ export const AVAILABLE_LANGUAGES = [
   { code: "pt", label: "Portuguese" },
   { code: "ja", label: "Japanese" },
   { code: "no", label: "Norwegian" },
-  { code: "en", label: "English" }
+  { code: "en", label: "English" },
   { code: "ar", label: "Arabic" }
 ];
