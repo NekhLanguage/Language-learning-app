c3f99b6ce8376e9750e87b0abdd65b55d394ea63
NekhLanguage <nekhbrazil@gmail.com>
2026-03-12 12:36:58 +0100

    98716

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import { AVAILABLE_LANGUAGES } from "./languages.js?v=0.9.87.15";
+import { AVAILABLE_LANGUAGES } from "./languages.js?v=0.9.87.16";
 import { speak, setTTS, speakSentenceOnLoad } from "./audioengine.js";
  let USER = null;
 document.addEventListener("DOMContentLoaded", () => {
-  const APP_VERSION = "v0.9.87.15";
+  const APP_VERSION = "v0.9.87.16";
   const MAX_LEVEL = 7;
   const DEV_START_AT_LEVEL_7 = false; // set false after stress testing
-  const CONTENT_VERSION = 2;
+  const CONTENT_VERSION = 3;
 
   const startScreen = document.getElementById("start-screen");
   const learningScreen = document.getElementById("learning-screen");
@@ -68,7 +68,9 @@ loadUser();
     // Bundle 8
     "ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN",
     // Bundle 9
-    "my","Syour","her","his","our","their"
+    "MY","YOUR","HER","HIS","OUR","THEIR",
+    // Bundle 10
+    "GIRL","BOY","WOMAN","MAN","BE"
 ];
 // --------------------
 // Support Language UI (Abbreviation + Native Name)
