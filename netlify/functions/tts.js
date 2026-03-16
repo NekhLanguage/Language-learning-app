464fa93eaa7fb9f872c01a5528b8c58b6726c9db
NekhLanguage <nekhbrazil@gmail.com>
2026-03-16 17:35:30 +0100

    Update tts.js

--- netlify/functions/tts.js
+++ netlify/functions/tts.js
@@ -1,9 +1,31 @@
-exports.handler = async function () {
+const textToSpeech = require("@google-cloud/text-to-speech");
+
+const client = new textToSpeech.TextToSpeechClient({
+  credentials: JSON.parse(process.env.GOOGLE_TTS_KEY)
+});
+
+exports.handler = async (event) => {
+  const { text, lang } = JSON.parse(event.body);
+
+  const request = {
+    input: { text },
+    voice: {
+      languageCode: lang,
+      ssmlGender: "NEUTRAL"
+    },
+    audioConfig: {
+      audioEncoding: "MP3"
+    }
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+
   return {
     statusCode: 200,
     headers: {
-      "Content-Type": "text/plain"
+      "Content-Type": "audio/mpeg"
     },
-    body: "TTS function working"
+    body: response.audioContent.toString("base64"),
+    isBase64Encoded: true
   };
 };
