23dcc331f0d096f1a8f17aa7dddc5d63f72343da
NekhLanguage <nekhbrazil@gmail.com>
2026-03-21 10:22:01 +0100

    supa

--- netlify/functions/loadUser.js
+++ netlify/functions/loadUser.js
@@ -1,25 +1,28 @@
-const fs = require("fs");
-const path = require("path");
+const fetch = require("node-fetch");
 
-const DATA_FILE = path.join("/tmp", "users.json");
+const SUPABASE_URL = "https://miprvzsfunbmjippzrxf.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pcHJ2enNmdW5ibWppcHB6cnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwODA1NjMsImV4cCI6MjA4OTY1NjU2M30.78ONiXxrznbsAw-bEX_haMmrbRoV5t6vkfxzzwIw0lc";
 
 exports.handler = async (event) => {
 
   const { email } = JSON.parse(event.body);
 
-  if (!fs.existsSync(DATA_FILE)) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ user: null })
-    };
-  }
+  const res = await fetch(
+    `${SUPABASE_URL}/rest/v1/users?email=eq.${email}`,
+    {
+      headers: {
+        "apikey": SUPABASE_KEY,
+        "Authorization": `Bearer ${SUPABASE_KEY}`
+      }
+    }
+  );
 
-  const data = JSON.parse(fs.readFileSync(DATA_FILE));
+  const data = await res.json();
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      user: data[email] || null
+      user: data[0]?.data || null
     })
   };
 };
--- netlify/functions/saveUser.js
+++ netlify/functions/saveUser.js
@@ -1,21 +1,25 @@
-const fs = require("fs");
-const path = require("path");
+const fetch = require("node-fetch");
 
-const DATA_FILE = path.join("/tmp", "users.json");
+const SUPABASE_URL = "https://miprvzsfunbmjippzrxf.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pcHJ2enNmdW5ibWppcHB6cnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwODA1NjMsImV4cCI6MjA4OTY1NjU2M30.78ONiXxrznbsAw-bEX_haMmrbRoV5t6vkfxzzwIw0lc";
 
 exports.handler = async (event) => {
 
   const { email, user } = JSON.parse(event.body);
 
-  let data = {};
-
-  if (fs.existsSync(DATA_FILE)) {
-    data = JSON.parse(fs.readFileSync(DATA_FILE));
-  }
-
-  data[email] = user;
-
-  fs.writeFileSync(DATA_FILE, JSON.stringify(data));
+  await fetch(`${SUPABASE_URL}/rest/v1/users`, {
+    method: "POST",
+    headers: {
+      "apikey": SUPABASE_KEY,
+      "Authorization": `Bearer ${SUPABASE_KEY}`,
+      "Content-Type": "application/json",
+      "Prefer": "resolution=merge-duplicates"
+    },
+    body: JSON.stringify({
+      email,
+      data: user
+    })
+  });
 
   return {
     statusCode: 200,
