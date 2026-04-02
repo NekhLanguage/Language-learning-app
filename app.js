fbe722a34ccb734cec8565bfbfe402ddaf4b6548
NekhLanguage <nekhbrazil@gmail.com>
2026-04-02 11:37:11 +0200

    9996

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { AVAILABLE_LANGUAGES } from "./languages.js?v=0.9.99.5";
+import { AVAILABLE_LANGUAGES } from "./languages.js?v=0.9.99.6";
 import { speak, setTTS, speakSentenceOnLoad } from "./audioengine.js";
 const CORE_BUNDLES = [
 
@@ -167,7 +167,7 @@ const RESOURCE_PACKS = {
 }; 
 let USER = null;
 document.addEventListener("DOMContentLoaded", async () => {
-  const APP_VERSION = "v0.9.99.5";
+  const APP_VERSION = "v0.9.99.6";
   const MAX_LEVEL = 7;
   const DEV_START_AT_LEVEL_7 = false; // set false after stress testing
   const CONTENT_VERSION = 11;
